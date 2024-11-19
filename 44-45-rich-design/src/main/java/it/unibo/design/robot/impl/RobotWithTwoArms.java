63ef872425e5e0a5d3acc70e144b8f10a39adf22
EnricoAncaraniUnibo <enrico.ancarani@studio.unibo.it>
2024-11-19 13:30:11 +0100

    4.2

--- 44-45-rich-design/src/main/java/it/unibo/design/robot/impl/RobotWithTwoArms.java
+++ 44-45-rich-design/src/main/java/it/unibo/design/robot/impl/RobotWithTwoArms.java
@@ -10,6 +10,7 @@ public class RobotWithTwoArms extends BaseRobot implements RobotWithArms{
     private final BasicArm ba1;
     private final BasicArm ba2;
     private int items;
+    private final static double TRANSPORT_OBJECT_CONSUMPTION=0.1;
 
     public RobotWithTwoArms(final String name) {
         super(name);
@@ -18,21 +19,26 @@ public class RobotWithTwoArms extends BaseRobot implements RobotWithArms{
         items = 0;
     }
 
+    protected double getBatteryRequirementForMovement() {
+        return super.getBatteryRequirementForMovement() + getCarriedItemsCount() * TRANSPORT_OBJECT_CONSUMPTION;
+    }
+
     @Override
     public boolean pickUp() {
         if(items == 2) {
             return false;
         }
-        if(ba1.isGrabbing()) 
-        {
-            ba2.pickUp();
-            items++;
-            return true;
-        } else {
+        if(isBatteryEnough(ba1.getConsumptionForPickUp()) && !ba1.isGrabbing()) {
             ba1.pickUp();
             items++;
             return true;
         }
+        if(isBatteryEnough(ba2.getConsumptionForPickUp()) && !ba2.isGrabbing()) {
+            ba2.pickUp();
+            items++;
+            return true;
+        }
+        return false;
     }
 
     @Override
@@ -40,15 +46,17 @@ public class RobotWithTwoArms extends BaseRobot implements RobotWithArms{
         if(items == 0) {
             return false;
         } 
-        if(ba1.isGrabbing()) {
+        if(isBatteryEnough(ba1.getConsumptionForPickUp()) && ba1.isGrabbing()) {
             ba1.dropDown();
             items--;
             return true;
-        } else {
+        }
+        if(isBatteryEnough(ba2.getConsumptionForPickUp()) && ba2.isGrabbing()) {
             ba2.dropDown();
             items--;
             return true;
         }
+        return false;
     }
 
     @Override
